e6f1ec16ca4c7980a3161955509dad930e9bd7ea
Will Fleming <wfleming77@gmail.com>
2021-10-19 20:08:38 -0400

    Remove unneeded dependency

--- project.clj
+++ project.clj
@@ -1,7 +1,5 @@
 (defproject clojure-aws-lambda-example "0.1.0-SNAPSHOT"
-  :dependencies [[com.amazonaws/aws-lambda-java-core "1.2.1"]
-                 [com.amazonaws/aws-lambda-java-runtime-interface-client "2.0.0"]
-
-                 [org.clojure/clojure "1.10.1"]]
+  :dependencies [[com.amazonaws/aws-lambda-java-runtime-interface-client "2.0.0"]
+                 [org.clojure/clojure "1.10.3"]]
   :repl-options {:init-ns clojure-aws-lambda-example.core}
   :profiles {:uberjar {:aot :all}})
